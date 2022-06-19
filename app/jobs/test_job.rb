1ae16f6dcbcca794e28bbfee138ccbe006aa9c88
s-sakashi <suguru.sakashita@gmail.com>
2022-06-20 08:53:45 +0900

    This code would be repeat itself endlessly

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -4,6 +4,8 @@ class TestJob < ApplicationJob
   retry_on StandardError, attempts: 2, wait: 3.seconds
 
   def perform
-    raise StandardError
+    # raise StandardError
+    Rails.logger.warn("Test!!!!!!!!!!")
+    retry_job wait: 3.seconds, attempts: 2
   end
 end
