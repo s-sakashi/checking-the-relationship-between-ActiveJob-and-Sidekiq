d23d1ecd8bfcd6d1efb1d9036ffd08d620468785
s-sakashi <suguru.sakashita@gmail.com>
2022-06-20 09:39:56 +0900

    Default attempts number is 5, But this code would be repeat over 6 times.

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -1,11 +1,13 @@
 class TestJob < ApplicationJob
   queue_as :default
 
-  retry_on StandardError, attempts: 2, wait: 3.seconds
+  retry_on StandardError
 
   def perform
-    # raise StandardError
-    Rails.logger.warn("Test!!!!!!!!!!")
-    retry_job wait: 3.seconds, attempts: 2
+    raise StandardError
+    # このコードでも6回以上再試行された
+    # 考えうることとしては、、、
+    # -  毎回raise error のたびにリトライ（5回）が実行されるため、
+    #    同じエラーが出続ける限り無限に再試行される
   end
 end
