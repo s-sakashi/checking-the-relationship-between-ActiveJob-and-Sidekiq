292f5b98c60ae8f267f93743ede4b750cfac8ebb
s-sakashi <suguru.sakashita@gmail.com>
2022-06-20 11:03:00 +0900

    If setting sidekiq-retry: 3 & ActiveJob: 2, Job retry on total 5 times

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -1,12 +1,20 @@
+# sidekiq_options retry: 3
+# retry_on attempts: 2
+
 class TestJob < ApplicationJob
   queue_as :default
 
-  sidekiq_options retry: false
+  # sidekiq_options retry: false
 
-  retry_on StandardError, wait: 1.second, attempts: 2
+  retry_on StandardError, wait: 5.seconds, attempts: 2
 
   def perform
     raise StandardError, "sidekiq_options retry: false"
-    # 2回実行完了後にraise StandardError
+    # 合計 5 回実行された
+    # - 2回実行完了後にraise StandardError
+    #   - Sidekiqでは「予定」に追加され、
+    #   - 完了次第「完了」に追加される
+    # - Sidekiqで3回リトライ
+    #   - こちらは再試行で3回リトライ → 完了に+3
   end
 end
