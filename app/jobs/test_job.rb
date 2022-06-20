40be88fdeb72c67a3a4e7cdddc39f9ec64e59dc8
s-sakashi <suguru.sakashita@gmail.com>
2022-06-20 11:27:25 +0900

    If you want to retry job according to error, turn off sidekiq-options and set attempts-numner in retry-on options

--- app/jobs/test_job.rb
+++ app/jobs/test_job.rb
@@ -1,22 +1,21 @@
-# sidekiq_options retry: 3
-# sidekiq_options retry: false
-# retry_on attempts: 2
+# 条件に応じてリトライ回数を変えたい場合は以下のようにする
+# - sidekiq_options のリトライをfalseにする
+# - エラーに応じてretry_on attempts の数値を変える
+
+
+class RetryOnceError < StandardError; end
+class RetryTwiceError < StandardError; end
 
 class TestJob < ApplicationJob
   queue_as :default
 
   sidekiq_options retry: false
 
-  retry_on StandardError, wait: 5.seconds, attempts: 2
+  retry_on RetryOnceError, wait: 5.seconds, attempts: 1
+  retry_on RetryTwiceError, wait: 5.seconds, attempts: 2
 
   def perform
-    raise StandardError, "sidekiq_options retry: false, retry_on: 2"
-    # 合計 2 回実行された
-    # - 2回実行完了後にraise StandardError
-    #   - Sidekiqでは「予定」に追加され、
-    #   - 完了次第「完了」に追加される
-    # - Sidekiqではリトライされない
-    #   - 毎回raise error
+    raise RetryTwiceError, "sidekiq_options retry: false, retry_on: 2"
   end
 end
 
